a992492bede951c299a638a19781430c5d838db5
jz391 <128315339+jz391@users.noreply.github.com>
2023-07-06 13:07:12 -0400

    Create Main.lua

---
+++ Roblox/Places/Small_Projects/FirstProject/Sample_Scripts/Main.lua
@@ -0,0 +1,10 @@
+local rs = game:GetService("ReplicatedStorage")
+repeat wait() until game.Players.LocalPlayer.Character:FindFirstChild("Humanoid")
+
+game:GetService("RunService").RenderStepped:Connect(function()
+	local a = rs.Union:Clone()
+	a.Parent = game.Workspace
+	a.Position = Vector3.new(math.random(-23.126, 5.134),math.random(3.628, 21.143),math.random(-15.963, 12.882))
+	a.CFrame = CFrame.new(a.Position) * CFrame.Angles(math.random(),math.random(),math.random())
+	a.SurfaceLight.Color = Color3.new(math.random(), math.random(), math.random())
+end)
