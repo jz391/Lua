93956f1aecc9afef24b4aeffee54a53a20c14b5c
jz391 <128315339+jz391@users.noreply.github.com>
2023-09-28 00:51:50 -0400

    Create TeleportHandler.lua

---
+++ Roblox/Places/Small_Projects/ProjectHub/Sample Scripts/TeleportHandler.lua
@@ -0,0 +1,24 @@
+-- this script handles teleports to my other Roblox projects
+local TpServ = game:GetService("TeleportService")
+local notifRemote = game:GetService("ReplicatedStorage"):WaitForChild("PromptNotify")
+
+game:GetService("ProximityPromptService").PromptTriggered:Connect(function(promptObj, plr)
+	local id = promptObj:WaitForChild("ID").Value
+	
+	print("Attempting to teleport player ("..plr.Name..")\nPlace Name:", promptObj.Parent.SG.Text.Text, "\nPlace Id:", id)
+	notifRemote:FireClient(plr)
+	TpServ:Teleport(id, plr)
+end)
+
+local enumStrLength = tostring(Enum.TeleportResult):len()
+
+local failedTP = TpServ.TeleportInitFailed:Connect(function(plr, tpResult, errorMsg, tpOptions)
+	local errorEnum, tpOptions = nil
+	if tpResult ~= Enum.TeleportResult.Success then
+		tpResult = tostring(tpResult)
+		errorEnum = string.sub(tpResult, enumStrLength, tpResult:len())
+		errorMsg = errorMsg or "No other info given"
+	end
+	warn("Error teleporting", plr.name, "\nError enum:", errorEnum, "\nError message:", errorMsg)
+	notifRemote:FireClient(plr, errorEnum, errorMsg)
+end)
