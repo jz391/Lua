efdbabcbc64ddc5974eec93306564a14c241b6e1
jz391 <128315339+jz391@users.noreply.github.com>
2023-10-17 23:54:17 -0400

    Create gunShoot_Server.lua

---
+++ Roblox/Places/Small_Projects/WeaponCasting/Sample_Scripts/gunShoot_Server.lua
@@ -0,0 +1,41 @@
+local rs = game:GetService("ReplicatedStorage")
+local ss = game:GetService("ServerStorage")
+local debri = game:GetService("Debris")
+local pew = rs:FindFirstChild("pew") -- a remote event to be fired by client and received by server
+local plrdebounces = {}
+
+pew.OnServerEvent:Connect(function(plr, info)
+	if table.find(plrdebounces, plr.UserId) then 
+		plr:Kick("cooldown")
+		return
+	end
+	
+	local infotab = {info}
+	local userid = plr.UserId 
+	local index = table.insert(plrdebounces, userid)
+	
+	local rocfold = plr:FindFirstChild("RocFolder")	
+	if not rocfold or rocfold.Ammo.Value < 1 then
+		plr:Kick("only "..tostring(rocfold.Ammo.Value))
+	end
+	rocfold.Ammo.Value -= 1
+	
+	local gun = plr.Character:FindFirstChild("Gun").Handle
+	local bulet = ss.bullet:Clone()
+
+	if not gun then return end
+	bulet.plr.Value = plr.Name
+	
+	--[[
+	bulet.dirs.dirX.Value = infotab[1]
+	bulet.dirs.dirY.Value = infotab[2]
+	bulet.dirs.dirZ.Value = infotab[3]
+	]]
+	bulet.Position = gun.Position
+	
+	bulet.CFrame = CFrame.lookAt(bulet.Position, info)
+	bulet.Parent = game.Workspace
+		
+	wait(0.3)
+	table.remove(plrdebounces, index)
+end)
