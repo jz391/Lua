f29df1dc9269925cd5d96d4371e352af381cfea7
jz391 <128315339+jz391@users.noreply.github.com>
2023-11-05 19:19:15 -0500

    Update main.lua
    inserted old code in comments

--- Roblox/Places/FirstProject/Sample_Scripts/main.lua
+++ Roblox/Places/FirstProject/Sample_Scripts/main.lua
@@ -1,5 +1,5 @@
 local rs = game:GetService("ReplicatedStorage")
-repeat wait() until game.Players.LocalPlayer.Character:FindFirstChild("Humanoid")
+repeat wait() until game.Players.LocalPlayer.Character:FindFirstChild("Humanoid") -- wait for one player to fully load
 
 game:GetService("RunService").RenderStepped:Connect(function()
 	local a = rs.Union:Clone()
@@ -8,3 +8,13 @@ game:GetService("RunService").RenderStepped:Connect(function()
 	a.CFrame = CFrame.new(a.Position) * CFrame.Angles(math.random(),math.random(),math.random())
 	a.SurfaceLight.Color = Color3.new(math.random(), math.random(), math.random())
 end)
+
+[[
+originally was this old code, until it was recently updated
+
+while true do
+	local a = rs.Union:Clone()
+	a.Parent = game.Workspace
+	a.Position = Vector3.new(math.random(-23.126, 5.134),math.random(3.628, 21.143),math.random(-15.963, 12.882))
+end
+]]
